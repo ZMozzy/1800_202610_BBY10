a97ce90b4a1ca6ce9b9ec7549d9144683fd8d1d3
Zach M <zmosdell@my.bcit.ca>
2026-03-09 21:52:20 -0700

    changed things around

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
+console.log("main.js loaded");
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
 
 // If you have custom global styles, import them as well:
 // import '../styles/style.css';
-import {getAuth} from "firebase/auth";
+import {auth} from "./firebaseConfig.js"
 import {collection, query, where, getDocs} from "firebase/firestore";
 
-function sayHello() {
-
-}
 document.getElementById("getstarted").addEventListener("click", function() {
     window.location.href = "landing.html";
 });
@@ -17,26 +16,8 @@ document.getElementById("loginbtn").addEventListener("click", function() {
     window.location.href = "login.html";
 });
 
-function updateWaitTime() {
-    let newTime = prompt("Enter new wait time:");
-    //only update if they didnt hit cancel or enter nothing
-    if (newTime !== null && newTime.trim() !== "") {
-        let numericValue = Number(newTime);
 
-        // Check if it is a valid integer and not a negative number
-        if (Number.isInteger(numericValue) && numericValue >= 0) {
-            let finalWait = numericValue + " min";
-            
-            document.getElementById("wait-time-value").innerText = finalWait;
-        }
-}
-}
-// goes to add.html
-function goToAdd() {
-    window.location.href = "add.html";
-}
 
-const auth = getAuth();
 const user = auth.currentUser;
 
 //checks if they are a user and if user has an active post
@@ -44,13 +25,13 @@ const user = auth.currentUser;
 async function goToProfile() {
     try {
         if (user) {
-             //change this one to be resturant post collection name!!!!!!!!!!!!!!
+             
             const restaurantRef = collection(db, "Restaurant");
              const q = query(restaurantRef, where("userId", "==", user.uid));
 
             const querySnapshot = await getDocs(q)
             if (!querySnapshot.empty) {
-                window.location.href = "profile.html";
+                window.location.href = "owner-profile.html";
             } else {
                 window.location.href = "user-profile.html";
             }
@@ -62,14 +43,14 @@ async function goToProfile() {
     }
 }
 
-//if login/sign up feature breaks it was probably this -zach was not here
-//for either login or sign up
-const btn = document.getElementById("loginForm") || document.getElementById("signinForm");
-//directs to profile page after login/sign up
-btn.addEventListener("submit", function(e) {
-    e.preventDefault(); // stops page refresh
 
-    // runs validation / login logic
+
+document.getElementById("profile-link").addEventListener("click", function(e) {
+    console.log("Clicked");
+    e.preventDefault();
     goToProfile();
 });
+
+
 // document.addEventListener('DOMContentLoaded', sayHello);
+
